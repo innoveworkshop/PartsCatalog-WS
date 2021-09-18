505031a766dd843056517d2833eff4f27f6b444c
Nathan Campos <eeepc904@gmail.com>
2021-09-18 17:23:26 +0100

    Added the ability to check for required parameters in URLs using XOR

--- src/com/innoveworkshop/partscatalog/servlets/utils/ServletParameterChecker.java
+++ src/com/innoveworkshop/partscatalog/servlets/utils/ServletParameterChecker.java
@@ -27,11 +27,11 @@ public class ServletParameterChecker {
 	}
 	
 	/**
-	 * Checks if all of the parameters specified are set.
+	 * Checks if a parameter specified is set.
 	 * WARNING: This function will send a HTTP error automatically.
 	 * 
 	 * @param  parameter Parameter to be checked.
-	 * @return           True if the parameters is present.
+	 * @return           True if the parameter is present.
 	 * 
 	 * @throws IOException If something goes wrong when replying to the client.
 	 */
@@ -58,6 +58,25 @@ public class ServletParameterChecker {
 		for (String parameter : parameters) {
 			if (!require(parameter))
 				return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Checks if only one of the two parameters is set.
+	 * WARNING: This function will send a HTTP error automatically.
+	 * 
+	 * @param  param1 First parameter to be checked.
+	 * @param  param2 Second parameter to be checked.
+	 * @return        True if only one of the parameters is set.
+	 * 
+	 * @throws IOException If something goes wrong when replying to the client.
+	 */
+	public boolean requireXOR(String param1, String param2) throws IOException {
+		if (!((request.getParameter(param1) != null) ^ (request.getParameter(param2) != null))) {
+			response.sendError(422, "Unprocessable Entity");
+			return false;
 		}
 		
 		return true;
