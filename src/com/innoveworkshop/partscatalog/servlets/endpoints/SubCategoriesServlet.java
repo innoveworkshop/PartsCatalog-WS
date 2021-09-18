030720f725abedb2138f5c522955238bfe175358
Nathan Campos <eeepc904@gmail.com>
2021-09-18 17:47:04 +0100

    Implemented all of the operations for the sub-categories endpoint

--- src/com/innoveworkshop/partscatalog/servlets/endpoints/SubCategoriesServlet.java
+++ src/com/innoveworkshop/partscatalog/servlets/endpoints/SubCategoriesServlet.java
@@ -14,8 +14,11 @@ import org.hibernate.Session;
 
 import com.innoveworkshop.partscatalog.config.Configuration;
 import com.innoveworkshop.partscatalog.db.DatabaseConnection;
+import com.innoveworkshop.partscatalog.db.models.Category;
 import com.innoveworkshop.partscatalog.db.models.SubCategory;
 import com.innoveworkshop.partscatalog.servlets.utils.FormattableCollection;
+import com.innoveworkshop.partscatalog.servlets.utils.FormattableMessage;
+import com.innoveworkshop.partscatalog.servlets.utils.ServletParameterChecker;
 import com.innoveworkshop.partscatalog.servlets.utils.ServletResponseFormatter;
 
 /**
@@ -44,29 +47,101 @@ public class SubCategoriesServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletResponseFormatter formatter = new ServletResponseFormatter(request, response);
+		
 		// Check if we have the required parent category XOR ID parameter.
-		if (!((request.getParameter("parent") != null) ^ (request.getParameter("id") != null))) {
-			response.sendError(422, "Unprocessable Entity");
+		ServletParameterChecker paramChecker = new ServletParameterChecker(request, response);
+		if (!paramChecker.requireXOR("category", "id"))
 			return;
-		}
 		
 		// List sub-categories.
 		Query query;
-		if (request.getParameter("parent") != null) {
+		if (request.getParameter("category") != null) {
 			// Get sub-categories from a category.
 			query = session.createQuery("FROM SubCategory WHERE parentCategory.id = :parent");
-			query.setParameter("parent", Integer.parseInt(request.getParameter("parent")));
+			query.setParameter("parent", Integer.parseInt(request.getParameter("category")));
+			
+			formatter.setVerbose(false);
 		} else {
 			// Get a single sub-category.
 			query = session.createQuery("FROM SubCategory WHERE id = :id");
 			query.setParameter("id", Integer.parseInt(request.getParameter("id")));
+			
+			formatter.setVerbose(true);
 		}
 		@SuppressWarnings("unchecked")
 		List<SubCategory> subCategories = (List<SubCategory>)query.getResultList();
 		
+		// Respond to the request.
+		formatter.respond(new FormattableCollection("subcategories", subCategories));
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletParameterChecker paramChecker = new ServletParameterChecker(request, response);
+		SubCategory subCategory = null;
+
+		// Check if we are editing or adding.
+		if (request.getParameter("id") == null) {
+			// Create a brand new one.
+			subCategory = new SubCategory();
+		} else {
+			// Get the object from the database.
+			subCategory = (SubCategory)session.get(SubCategory.class,
+					Integer.parseInt(request.getParameter("id")));
+			
+			// Check if it exists.
+			if (subCategory == null) {
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
+				return;
+			}
+		}
+		
+		// Check for required parameters.
+		if (!paramChecker.requireAll("name", "category"))
+			return;
+		
+		// Check if the parent category exists.
+		Category category = (Category)session.get(Category.class,
+				Integer.parseInt(request.getParameter("category")));
+		if (category == null) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return;
+		}
+		
+		// Update the object and commit changes.
+		subCategory.setName(request.getParameter("name"));
+		subCategory.setParentCategory(category);
+		session.saveOrUpdate(subCategory);
+		
 		// Setup the response formatter and respond to the request.
 		ServletResponseFormatter formatter = new ServletResponseFormatter(request, response);
 		formatter.setVerbose(true);
-		formatter.respond(new FormattableCollection("subcategories", subCategories));
+		formatter.respond(subCategory);
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Check if we have the required ID parameter.
+		ServletParameterChecker paramChecker = new ServletParameterChecker(request, response);
+		if (!paramChecker.require("id"))
+			return;
+		
+		// Get the object from the database and check if it exists.
+		SubCategory subCategory = (SubCategory)session.get(SubCategory.class,
+				Integer.parseInt(request.getParameter("id")));
+		if (subCategory == null) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return;
+		}
+		
+		// Delete the entry and commit the changes.
+		session.delete(subCategory);
+		session.getTransaction().commit();
+		
+		// Setup the response formatter and respond to the request.
+		ServletResponseFormatter formatter = new ServletResponseFormatter(request, response);
+		formatter.setVerbose(true);
+		formatter.respond(new FormattableMessage("Deleted successfully"));
 	}
 }
